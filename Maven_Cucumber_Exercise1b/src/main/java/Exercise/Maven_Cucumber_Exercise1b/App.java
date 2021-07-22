9cd35f843635a8cba1249bd12d4a336477201bd9
ferreiracamilo <camiloferreira2009@hotmail.com>
2021-07-22 16:34:02 -0300

    First commit Uploading All files

---
+++ Maven_Cucumber_Exercise1b/src/main/java/Exercise/Maven_Cucumber_Exercise1b/App.java
@@ -0,0 +1,13 @@
+package Exercise.Maven_Cucumber_Exercise1b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
